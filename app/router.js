f95d32d5ec0e69de244f0a99020c53c8200f47ba
ShiChao1996 <2483061998@qq.com>
2017-09-20 23:18:23 +0800

    admin 登陆接口

--- app/router.js
+++ app/router.js
@@ -1,6 +1,7 @@
 'use strict';
 
 module.exports = app => {
+  app.post('/admin/login', 'admin.login');
   app.post('/article/update', 'article.update');
   app.get('/article/getlist', 'article.getList');
   app.get('/article/getdetail', 'article.getDetail');
